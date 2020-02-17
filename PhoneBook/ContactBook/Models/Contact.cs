af0c9443c0ce375d2225b1c906e11511cb54c94d
Thembi Tsheme <nothemba@bbd.co.za>
2020-02-17 20:45:21 +0200

    initail commit

---
+++ PhoneBook/ContactBook/Models/Contact.cs
@@ -0,0 +1,30 @@
+namespace ContactBook.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Contact
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+        [StringLength(50)]
+        public string CellNumber { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string Address { get; set; }
+    }
+}
